c7425eaef3c0330231540619a5e92138642856f5
Jonathan Watmough <jonathan.watmough@gmail.com>
2022-05-07 20:09:23 -0500

    Create line-reader.cpp

---
+++ line-reader.cpp
@@ -0,0 +1,106 @@
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/*
+ *  reader
+ *
+ *  initialize with a file
+ *  call to return a null terminated line
+ *  line separators are lf, crlf, cr
+ *  we'll read a lf, or cr followed by lf, or
+ *  standalone cr
+ */
+
+// lazy line reader
+struct reader {
+  std::string _filename;
+  std::vector<char> _buffer;
+  std::vector<char>::iterator _pos;
+  std::vector<char>::iterator _end;
+  std::vector<char>::iterator _line;
+  bool _badchar;
+
+  // theres another line to return
+  bool hasline() { return _pos != _end; }
+
+  reader() = delete;
+  reader(std::string &&filename) : _filename(filename) {
+    // get file size
+    std::fstream file(_filename,  std::fstream::in |
+                                  std::fstream::ate |
+                                  std::fstream::binary);
+    auto filesize = long(file.tellg());
+    auto buffersize = filesize + 1;
+    // create buffer
+    _buffer = std::vector<char>(buffersize);
+    // seek to begin and read file
+    file.seekg(std::fstream::beg);
+    file.read(&_buffer[0], filesize);
+    // setup iterators
+    _pos = _line = _buffer.begin();
+    _end = _buffer.end();
+  };
+
+  // find next line in zero terminated buffer
+  std::vector<char>::iterator findnextline() {
+    while (1) {
+      // read chars from pos until \0 lf crlf cr error
+      while (*_pos > 0xD) {
+        // leave pos on \0 \a \d or bad character
+        ++_pos;
+      }
+      // if we read a cr, skip over single following lf
+      switch (*_pos) {
+      case 0x0: // end of buffer
+      case 0xA: // terminate at lf or lf part of crlf
+        *_pos++ = '\0';
+        return _pos;
+      case 0xD: // terminate at cr or crlf
+        *_pos++ = '\0';
+        if (*_pos != 0xA) {
+          // not a lf, return position of next line
+          return _pos;
+        }
+        // otherwise, terminate at lf of crlf and bump _pos
+        *_pos++ = '\0';
+        return _pos;
+      default: // bad character, just skip over it
+        _badchar |= true;
+        ++_pos;
+      }
+    }
+    return _pos;
+  }
+
+  // getline
+  // _pos always points at nextline
+  // when called, we need to terminate the line, and be ready
+  // with the next line.
+  std::vector<char>::iterator getline() {
+    // set current line to pos
+    _line = _pos;
+    // at end of buffer? no more lines, return nullptr
+    if (_line == _end)
+      return static_cast<std::vector<char>::iterator>(nullptr);
+    // move _pos to start of next line
+    _pos = findnextline();
+    // return current line
+    return _pos;
+  }
+};
+
+int main() {
+  std::cout << "started" << std::endl;
+  
+  reader linereader("./las-files/1052127278.las");
+
+  while (linereader.hasline()) {
+    auto line = linereader.getline();
+    std::cout << (char *)&(*line) << std::endl;
+  };
+
+std::cout << "done" << std::endl;
+}
